5c1eef745239df75c64c5965369520ab55dfcca7
rushikeshadhav <adhavrushikesh6@gmail.com>
2022-08-03 13:49:18 +0530

    nav input width reduce

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ function animateValue(obj, start, end, duration) {
 const obj = document.getElementById("value-400");
 const obj2 = document.getElementById("value-400000");
 const obj3 = document.getElementById("value-10000");
-const obj4 = document.getElementById("value-20000");
+const obj4 = document.getElementById("value-200");
 animateValue(obj, 0, 400, 2000);
 animateValue(obj2, 0, 400000, 2000);
 animateValue(obj3, 0, 10000, 2000);
-animateValue(obj4, 0, 20000, 2000);
+animateValue(obj4, 0, 200, 2000);
 
 
 
